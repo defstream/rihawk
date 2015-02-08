666986c3987e87f69a7ebeda7026a91fa7946135
Hector Gray <hector.gray@vixlet.com>
2015-02-08 15:04:59 -0800

    +

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rihawk
 A streaming riakpbc wrapper with advanced features. 
+
+CURRENTLY IN PROGRESS
--- test/get-test.js
+++ test/get-test.js
@@ -1,15 +1,53 @@
+var mocha = require('mocha');
+var chai = require("chai");
+var expect = chai.expect;
+
 var RiakPBC = require('riakpbc');
 var riak = RiakPBC.createClient();
 
-var Get = require('../lib/streams/get');
-new Get({
-  client: riak,
-  bucket: ['nfl_team', 'fake_bucket'],
-  keys: ['SD', 'MIA', 'CHI', 'OAK','FAKETEAM', 'ATL']
-}).on('data', function(data) {
-  console.log('#data', data);
-}).on('error', function(err) {
-  console.log('#error', err);
-}).on('end', function() {
-  console.log('#end');
+
+describe('Get', function() {
+  var Get = require('../lib/streams/get');
+  it('get(bucket,key)', function(done) {
+    new Get({
+      client: riak,
+      bucket: 'nfl_team',
+      keys: 'SD'
+    }).on('data', function(data) {
+      console.log('#data', data);
+    }).on('error', function(err) {
+      console.log('#error', err);
+    }).on('end', function() {
+      console.log('#end');
+      done();
+    });
+  });
+  it('get(bucket,[key])', function(done) {
+    new Get({
+      client: riak,
+      bucket: ['nfl_team'],
+      keys: ['SD', 'MIA', 'CHI', 'OAK', 'FAKETEAM', 'ATL']
+    }).on('data', function(data) {
+      console.log('#data', data);
+    }).on('error', function(err) {
+      console.log('#error', err);
+    }).on('end', function() {
+      console.log('#end');
+      done();
+    });
+  });
+  it('get([bucket],[key])', function(done) {
+    new Get({
+      client: riak,
+      bucket: ['nfl_team', 'fake_bucket'],
+      keys: ['SD', 'MIA', 'CHI', 'OAK', 'FAKETEAM', 'ATL']
+    }).on('data', function(data) {
+      console.log('#data', data);
+    }).on('error', function(err) {
+      console.log('#error', err);
+    }).on('end', function() {
+      console.log('#end');
+      done();
+    });
+  });
 });
